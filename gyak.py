f904fbf26b9e0702b8902e7db3e8bd0b1cbd9630
Vanessza02 <janosikvanessza@gmail.com>
2024-12-01 13:46:02 +0100

    mainpy hozzaadva

---
+++ gyak.py
@@ -0,0 +1,14 @@
+def faktorialis(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * faktorialis(n - 1)
+
+try:
+    szam = int(input("Adj meg egy pozitív egész számot: "))
+    if szam < 0:
+        print("A faktoriális csak nemnegatív egész számokra értelmezett!")
+    else:
+        print(f"A {szam}! értéke: {faktorialis(szam)}")
+except ValueError:
+    print("Kérlek, egész számot adj meg!")
